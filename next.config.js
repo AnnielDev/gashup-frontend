cc747eeb8e97048acedbc73ed2ee2a80221d9b4c
lRomanti01 <romanti159@gmail.com>
2025-02-27 21:23:46 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
     domains: ["firebasestorage.googleapis.com"],
     remotePatterns: [
       {
