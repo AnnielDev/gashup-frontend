ca4298cdeed04cf0f98648fee1a05bd75e6bf166
lRomanti01 <romanti159@gmail.com>
2025-02-27 21:00:07 -0500

    fix prod link

--- utils/http.ts
+++ utils/http.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const http = axios.create({
   baseURL:
-    window.location.hostname === 'localhost'
+    window.location.hostname === "localhost"
       ? "http://localhost:3006/api/"
-      : 'https://gashup-api-1-klou9083q-jeferson-mercedes-projects.vercel.app/api/',
+      : "https://gashup-api-1.vercel.app/api/",
 });
 
 export default http;
